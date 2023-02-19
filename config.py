a631dec656d6959ca600c538c3d771361eb4fb25
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-02-19 19:20:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,7 @@ API_ID = "8858279"
 API_HASH = "ef28c3f458143cbcb4271a98a2e9d596"
 BOT_TOKEN = "5970291834:AAEJAXjXCjWGGdFYPxwkT3GRGVsu8O1YE3o"
 
-MONGO_DB_URI = "mongodb+srv://web:test@cluster0.zz54kvj.mongodb.net/?retryWrites=true&w=majority"
-
+MONGO_DB_URI = "mongodb+srv://480p:encode@cluster0.7fgwrif.mongodb.net/?retryWrites=true&w=majority"
 INDEX_ID = -1001868746379
 UPLOADS_ID = -1001855239636
 
